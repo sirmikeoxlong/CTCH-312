8ec70ff6ea9f812b233b1dbac68ba510798d833d
Bernadette <157823771+ursaBear707@users.noreply.github.com>
2026-03-17 22:41:20 -0500

    test init push

---
+++ Scenes/library/portals_library.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var entered
+
+func _on_body_entered(body: CharacterBody2D) -> void:
+	entered = true
+
+func _on_body_exited(body: Node2D) -> void:
+	entered = false
+	
+func _process(delta: float) -> void:
+	if entered == true:
+		if Input.is_action_just_pressed("ui_accept"):
+			get_tree().change_scene_to_file("res://Scenes/chapel_scene.tscn")
